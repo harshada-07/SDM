35b6929b83bd0a2a1489280113d12a8cf4b837fb
harshada-07 <harshada3678@gmail.com>
2024-11-28 18:33:41 +0530

    React-Application

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{color: 'pink'}}>Hello Harsha :)</h1>
+      <p>In today's fast-paced software development landscape, Continuous Integration (CI) and Continuous Deployment (CD)  have become essential practices for teams aiming to deliver high-quality software rapidly and efficiently. CI is the practice of automatically testing and integrating code changes into a shared repository multiple times a day. This approach helps identify integration issues early, reducing the time spent on debugging and allowing developers to focus on writing new features.
+          CD takes CI a step further by automating the deployment of code changes to production environments. This means that once code passes all tests in the CI pipeline, it can be automatically deployed to production without manual intervention. This not only accelerates the release cycle but also ensures that the software is always in a deployable state.
+      </p>
     </div>
-  );
+  )
 }
 
 export default App;
